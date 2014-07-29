5056142f1487add9a9368a4cf2ac7275d1e2d4b9
Ben <twelve1six@naver.com>
2014-07-29 15:22:56 +0900

    edit examples

--- views/get.php
+++ views/get.php
@@ -1,6 +1,9 @@
 <article>
-	<h1><?=$topic->tilte?></h1>
+	<h1><?=htmlspecialchars($topic->title)?></h1>
 	<div>
 		<?=$topic->description?>
 	</div>
 </article>
+
+<!-- 위의 예제에서 <?=$topic->title?> 은 <?=htmlspecialchars($topic->title)?>
+	으로 처리해야 한다.  -->
--- views/topic.list.php
+++ views/topic.list.php
@@ -1,12 +1,10 @@
 <ul>
 <?php
-foreach($topics as $entry){
+foreach($reports as $entry){
 ?>
-    <li><a href="/index.php/topic/get/<?=$entry->id?>"><?=$entry->title?></a></li>
+    <li><a href="/index.php/HReports/get/<?=$entry->id?>"><?=$entry->title?></a></li>
 <?php
 }
 ?>
 </ul>
 
-<!-- 위의 예제에서 <?=$topic->title?> 은 <?=htmlspecialchars($topic->title)?>
-	으로 처리해야 한다.  -->
